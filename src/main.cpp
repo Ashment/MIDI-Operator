1c3265b13d8c55a10bfbfc316a7fffaeb092f2c5
David Hou <gundamme@gmail.com>
2018-09-02 20:42:53 -0700

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include <Arduino.h>
+
+static const unsigned int LED = 13;
+
+void setup() {
+    // put your setup code here, to run once:
+    pinMode(LED, OUTPUT);
+}
+
+void loop() {
+    // put your main code here, to run repeatedly:
+    digitalWrite(LED, HIGH);
+    usbMIDI.sendNoteOn(60, 100, 1);
+    delay(1500);
+    digitalWrite(LED, LOW);
+    usbMIDI.sendNoteOff(60, 100, 1);
+    delay(1500);
+}
